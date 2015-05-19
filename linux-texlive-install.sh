c1d1660f1de0f1c6aa7d66a3b8e49958769c9476
Mikhail Veltishchev <dichlofos-mv@yandex.ru>
2015-05-19 12:24:53 +0300

    Prevent circular references creation during install

--- linux-texlive-install.sh
+++ linux-texlive-install.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-sudo ln -sf $(pwd)/mpl /usr/share/texlive/texmf-dist/metapost/mpl
-sudo ln -sf $(pwd)/dmvn /usr/share/texmf/tex/dmvn
+mpl_dest="/usr/share/texlive/texmf-dist/metapost/mpl"
+[ -L "$mpl_dest" ] && sudo unlink "$mpl_dest"
+sudo ln -sf $(pwd)/mpl "$mpl_dest"
+
+dmvn_dest="/usr/share/texmf/tex/dmvn"
+[ -L "$dmvn_dest" ] && sudo unlink "$dmvn_dest"
+sudo ln -sf $(pwd)/dmvn "$dmvn_dest"
+
 sudo mktexlsr
